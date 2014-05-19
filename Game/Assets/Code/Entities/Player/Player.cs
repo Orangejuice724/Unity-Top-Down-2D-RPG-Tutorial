032bfa763e4ba49eb3931b06067aee1a21aede24
Orangejuice724 <xXSniper1313Xx1313@gmail.com>
2014-05-19 20:27:32 +0930

    Episode 9

--- Game/Assets/Code/Entities/Player/Player.cs
+++ Game/Assets/Code/Entities/Player/Player.cs
@@ -5,6 +5,17 @@ public class Player : Entity {
 
 	public ItemPlayerManager ipManager;
 
+	public Sprite forward;
+	public Sprite backward;
+	public Sprite left;
+	public Sprite right;
+	public SpriteRenderer spriteParent;
+
+	public SpriteRenderer weaponSpriteParent;
+	public Vector2 forwardPos, backPos, sidePos;
+
+	public int direction;
+
 	void Start () {
 		ipManager.addToItemInventory(0, 5);
 	}
@@ -14,18 +25,55 @@ public class Player : Entity {
 		if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
 		{
 			rigidbody2D.transform.position += Vector3.up * speed * Time.deltaTime;
+			direction = 1;
 		}
 		if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
 		{
 			rigidbody2D.transform.position += Vector3.down * speed * Time.deltaTime;
+			direction = 0;
 		}
 		if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
 		{
 			rigidbody2D.transform.position += Vector3.left * speed * Time.deltaTime;
+			direction = 2;
 		}
 		if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
 		{
 			rigidbody2D.transform.position += Vector3.right * speed * Time.deltaTime;
+			direction = 3;
+		}
+
+		if(direction == 0)
+		{
+			spriteParent.sprite = forward;
+			weaponSpriteParent.sortingOrder = 21;
+			Quaternion newRot = Quaternion.Euler(0, 0, 0);
+			weaponSpriteParent.transform.localRotation = newRot;
+			weaponSpriteParent.transform.localPosition = forwardPos;
+		}
+		if(direction == 1)
+		{
+			spriteParent.sprite = backward;
+			weaponSpriteParent.sortingOrder = 19;
+			Quaternion newRot = Quaternion.Euler(0, 0, 90);
+			weaponSpriteParent.transform.localRotation = newRot;
+			weaponSpriteParent.transform.localPosition = backPos;
+		}
+		if(direction == 2)
+		{
+			spriteParent.sprite = left;
+			weaponSpriteParent.sortingOrder = 19;
+			Quaternion newRot = Quaternion.Euler(0, 0, 90);
+			weaponSpriteParent.transform.localRotation = newRot;
+			weaponSpriteParent.transform.localPosition = sidePos;
+		}
+		if(direction == 3)
+		{
+			spriteParent.sprite = right;
+			weaponSpriteParent.sortingOrder = 21;
+			Quaternion newRot = Quaternion.Euler(0, 0, 0);
+			weaponSpriteParent.transform.localRotation = newRot;
+			weaponSpriteParent.transform.localPosition = sidePos;
 		}
 
 		if(health <= 0)
